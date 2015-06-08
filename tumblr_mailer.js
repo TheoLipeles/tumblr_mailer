5fd5c1a8a6fdda82e297fef6c0cb93db99d1eb0e
Theo Lipeles <theo@lipeles.com>
2015-06-07 23:32:29 -0400

    removed api keys

--- tumblr_mailer.js
+++ tumblr_mailer.js
@@ -4,13 +4,13 @@ var ejs = require("ejs");
 // Authenticate via OAuth
 var tumblr = require('tumblr.js');
 var client = tumblr.createClient({
-  consumer_key: 'HVTTfY2BZe3wKULcwGqAs8o4foLihYiGrewWhOF32Q81i66I2Q',
-  consumer_secret: 'LQAPmknyimqIa4k79LMKWQkvOkeUdLcxlFo6MtApzzYah1DlRd',
-  token: 'Rh0wGtJYEVXVNEaGfpZUzGJTzZ8VLa91apnhjZNd1iXuuvsD6k',
-  token_secret: 'ANe4xXLSzhsIgQno7VHiPOJfQEylKmuPzbz4vEQRGXfSv8vXGG'
+  consumer_key: '',
+  consumer_secret: '',
+  token: '',
+  token_secret: ''
 });
 var mandrill = require('mandrill-api/mandrill');
-var mandrill_client = new mandrill.Mandrill('OWb8rSBjxLr7_NtEngEZLQ');
+var mandrill_client = new mandrill.Mandrill('');
 
 // Make the request
 client.userInfo(function (err, data) {
